8fc835ee09fd8b26e5fbc662ae9e4c836739057b
DomBennett <dominic.john.bennett@gmail.com>
2013-05-25 14:54:24 +0100

    Added Phylogeny Community Generator Functions

--- EcoDataTools.R
+++ EcoDataTools.R
@@ -1,8 +1,10 @@
-# No copyright, no warranty
-# Author: Dominic John Bennett
-# 03/05/2013
-# Library of functions created for my Masters project (MRes Biodiversity Informatics
-#  and Genomics , Imperial College London) of an ecological and phylogenetic slant
+## No copyright, no warranty
+## Dominic John Bennett
+## 25/05/2013
+## Library of functions created for my Masters project (MRes Biodiversity Informatics
+##  and Genomics , Imperial College London) of an ecological and phylogenetic slant
+
+## Coordinate manipulation
 
 DegToDec <- function(data) {
   # Take 'coordinate strings' in degrees (D M (S)) and convert to decimals.
@@ -83,22 +85,30 @@ DegToDec <- function(data) {
   return(decimals)
 }
 
-plotComm <- function(comm.data, phylo, groups){
+## Phylogenetic Community Analysis
+
+## Libraries
+library(picante)
+library(caper)
+library(lme4)
+
+## Function
+plotComm <- function(comm.data, phylo, groups = rep(1, nrow(comm.data))){
   # Plot community data on community phylogeny to visualise
   #  differences in community structure. Use colours to distinguish
-  #  site groups and alpha to distinguish abundances (like rainbow())
+  #  site groups and alpha to distinguish abundances (works like rainbow())
   #
   # Args:
-  #   comm.data: community data matrix (cols taxa, rows sites)
-  #   phylo: community phylogeny
-  #   groups: site groups
+  #  comm.data: community data matrix (cols taxa, rows sites)
+  #  phylo: community phylogeny
+  #  groups: site groups
   #
-  # Returns:
-  #   a matrix of community data
+  # Return:
+  #  a matrix of community data
   
   # plot phylogeny, allow space for points
-  variable.max <- (nrow(comm.data) * 10) + 150
-  variable.max <- ifelse(variable.max > 200, variable.max, 200) #min is 200
+  variable.max <- (nrow(comm.data) * 10) #+ 150
+  #variable.max <- ifelse(variable.max > 200, variable.max, 200) #min is 200
   plot(phylo, no.margin = T, show.tip.label = F, x.lim = c(0, variable.max))
   
   # generate alphas based on abundances
@@ -126,7 +136,7 @@ plotComm <- function(comm.data, phylo, groups){
 }
 
 randCommData <- function(phylo, nsites, nspp, pa = TRUE, lam = 0.1){
-  # Generates random community data for testing community phylogenetic
+  # Generate random community data for testing community phylogenetic
   #  methods
   #
   # Args:
@@ -136,7 +146,7 @@ randCommData <- function(phylo, nsites, nspp, pa = TRUE, lam = 0.1){
   #   pa: presence/absence data?
   #   lam: lambda for poisson distribution if pa is False
   #
-  # Returns:
+  # Return:
   #   a matrix of community data
   ntips <- length(phylo$tip.label)
   output <- matrix(rep(NA, ntips * nsites),
@@ -149,6 +159,97 @@ randCommData <- function(phylo, nsites, nspp, pa = TRUE, lam = 0.1){
   } else {
     for (i in 1:nsites) {
       output[i, ] <- sample(rpois(ntips, lam))
+    }
+  }
+  return(output)
+}
+
+split0 <- function(r = c(1, 10) , n = 2) {
+  # evenly split a range into n
+  #
+  # args:
+  #   r: range to be split, must be numerical, can be vector.
+  #   n: times to split r
+  #
+  # returns a vector of splits
+  output <- rep(NA, n)
+  output[1] <- 0
+  division <- 1/(n-1)
+  for (i in 2:n) {
+    output[i] <- output[i-1] + division
+  }
+  return(round((output * (max(r) - min(r))) + min(r)))
+}
+
+genCommData <- function(phylo, focal, nsites, nspp, fact = 1) {
+  # Generate clustered/overdispersed data for testing community
+  #  phylogenetic analyses.
+  #
+  # Args:
+  #  phylo - phylo.object, on which the community data will be based
+  #  focal - numeric index, indicating which tip to perform cluster/dispersion
+  #  nsites - the number of sites to create
+  #  nspp - the number of species to occur at each site
+  #  clust - if TRUE, data will be clustered else overdispersed
+  #  fact - scaling coefficient: larger the number the more pronounced the
+  #   effect by a power law (hence 0 not allowed)
+  #
+  # Return:
+  #  a matrix of community data
+  invertVector <- function(dists) {
+    u <- sort(unique(dists), TRUE)
+    s <- sort(u)
+    probs <- rep(NA, length(dists))
+    for (i in 1:length(u)) {
+      probs[u[i] == dists] <- s[i]
+    }
+    return (probs)
+  }
+  ntips <- length(phylo$tip.label)
+  output <- matrix(rep(NA, ntips * nsites),
+                   ncol = ntips, nrow = nsites)
+  colnames(output) <- phylo$tip.label
+  pd.dists <- cophenetic.phylo(phylo)
+  focal.dists <- pd.dists[ , focal] + 1 # avoid 0
+  if (fact > 0) focal.dists <- invertVector(focal.dists)
+  probs <- focal.dists^abs(fact)
+  probs <- probs/sum(probs)
+  for (i in 1:nsites) {
+    output[i, ] <- ifelse(1:ntips %in% sample(1:ntips, nspp,
+                                              prob = probs), 1, 0)
+  }
+  return (output)
+}
+
+evenCommData <- function(phylo, nsites, nspp) {
+  # Generate evenly distributed community phylogenetic data based on a
+  #  phylogeny. Takes the phylogeny, calculates cummulative PD from random
+  #  taxon, cuts at even intervals based on nspp to maximise distance.
+  #
+  # Args:
+  #  phylo - phylo.object, on which community will be based
+  #  nsites - number of sites
+  #  nspp - number of species
+  #
+  # Depends:
+  #  split0() - for evenly cutting up the cumsum.pd.dists
+  #
+  # Return:
+  #  a matrix of community data 
+  ntips <- length(phylo$tip.label)
+  output <- matrix(rep(0, ntips * nsites),
+                   ncol = ntips, nrow = nsites)
+  colnames(output) <- phylo$tip.label
+  pd.dists <- cophenetic.phylo(phylo)
+  for (i in 1:nsites) {
+    focal.pd.dists <- pd.dists[ , sample(1:ntips, 1)]
+    # select focal taxon at random
+    splits <- split0(1:sum(focal.pd.dists), nspp)
+    cumsum.pd.dists <- cumsum(focal.pd.dists)
+    for (j in 1:nspp) {
+      pull <- which(abs(cumsum.pd.dists - splits[j])
+                    == min(abs(cumsum.pd.dists - splits[j])))
+      output[i, pull] <- 1
     }
   }
   return(output)
