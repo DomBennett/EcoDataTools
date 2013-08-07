d206bcfb2779e88a3dd0448d3d5de24598dae992
DomBennett <dominic.john.bennett@gmail.com>
2013-08-07 18:44:54 +0100

    Updated null examples

--- cookbook/recipe1/generating_test_metadataR.R
+++ cookbook/recipe1/generating_test_metadataR.R
@@ -5,8 +5,12 @@
 ## Functions
 source("EcoDataTools.R")
 
+## Set seed
+set.seed(4)
+
 ## Dirs
 dir <- file.path('cookbook', 'recipe1')
+outdir <- file.path('wiki', 'cookbook')
 
 ## Import Tree
 big.phylo <- read.tree(file.path(dir, "big_phylo.txt"))
@@ -16,25 +20,25 @@ big.phylo <- read.tree(file.path(dir, "big_phylo.txt"))
 nstudies <- 30 # number of studies to generate
 nsites <- 40 # number of sites in each study
 nspecies <- 20 # the size of the phylgeny in each study
-nhtypes <- 2 # the number of habitat types to be compared
+nhstates <- 2 # the number of habitat states to be compared
+facts <- split0(-2:2, nhstates) # the facts between habitat states
+mean.incids <- sample(5:(nspecies/2), nhstates) # incidences between states
 
 # Running
 data <- list()
 phylos <- list()
 drop.n <- length(big.phylo$tip.label) - nspecies
-facts <- split0(-2:2, nhtypes)
-mean.incids <- split0(5:(nspecies/2), nhtypes)
 for (i in 1:nstudies) {
   study.phylo <- drop.tip(big.phylo, sample(big.phylo$tip.label, drop.n))
   study.phylo$edge.length <- study.phylo$edge.length *
     sample(c(0.01, 0.1, 1, 10, 100), 1)
   study.abun <- mean.incids * sample(c(1, 2, 3, 20, 100), 1)
   study.focal <- sample(1:nspecies, 1)
-  site.types <- sort(sample(1:nhtypes, nsites, replace = TRUE))
+  site.types <- sort(sample(1:nhstates, nsites, replace = TRUE))
   site.nsites <- table(site.types)
   site.data <- genCommData(study.phylo, study.focal, facts[1], mean.incids[1],
                            study.abun[1], site.nsites[1])
-  for (j in 2:nhtypes) {
+  for (j in 2:nhstates) {
     temp.site.data <- genCommData(study.phylo, study.focal, facts[j], mean.incids[j],
                                   study.abun[j], site.nsites[j])
     site.data <- rbind(site.data, temp.site.data)
@@ -46,15 +50,17 @@ for (i in 1:nstudies) {
   phylos <- c(phylos, list(study.phylo))
 }
 
-## Sanity check
 # plotting
 study.samples <- sample(1:nstudies, 4)
+png("r1_random_data_sc.png", width = 720)
 par(mfrow = c(2,2))
 for (i in 1:4) {
   plotComm(data[[study.samples[i]]], phylos[[study.samples[i]]],
            groups = rownames(data[[study.samples[i]]]))
 }
+dev.off()
 # sums
 #lapply(data, rowSums)
 #lapply(data, function(x) sum(x > 0))
 #lapply(phylos, function(x) sum(x$edge.length))
+save(list = c("data", "phylos"), file = file.path(dir, "test_metadata.RData"))
--- cookbook/recipe1/phylo_rarefaction.R
+++ cookbook/recipe1/phylo_rarefaction.R
@@ -2,9 +2,20 @@
 ## 30/06/2013
 ## Comparing phylogenetic diversity between sites
 
+## Functions
 source('EcoDataTools.R')
 
-# rarefy and calcualte PSE for each study
+## Set seed
+set.seed(4)
+
+## Dirs
+dir <- file.path('cookbook', 'recipe1')
+outdir <- file.path('wiki', 'cookbook')
+
+## Load
+load(file.path(dir, "test_metadata.RData"))
+
+## rarefy and calculate PSE for each study
 out <- list()
 for (i in 1:length(data)) {
   temp.data <- data[[i]]
@@ -16,7 +27,7 @@ for (i in 1:length(data)) {
   rownames(temp.data) <- NULL # phyloRarefy does not like non-unique names
   if (nhtypes > 1) {
     rarefied <- phyloRarefy(temp.data, temp.phylo, samp = 5, metric = 'PSE',
-                            nrands =10)[,1]
+                            nrands = 1000)[,1]
     # TODO (30/06/2013): Does not work with PD?
     names(rarefied) <- htypes
     out <- c(out, list(rarefied))
@@ -29,4 +40,8 @@ for (i in 1:length(data)) {
 pse.values <- rle(unlist(out))$values
 htypes <- factor(names(pse.values))
 par(mar = c(5, 4, 4, 2), mfrow = c(1,1))
+png("r1_pse_values.png", width = 720)
 plot(pse.values ~ htypes, xlab = "Habitat Type", ylab = "PSE")
+dev.off()
+
+# report mean differences
